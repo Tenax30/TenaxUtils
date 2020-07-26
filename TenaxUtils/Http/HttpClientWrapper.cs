d2d4b76dc4d0b347107045c5dc36f1ec8d315048
Tenax <mrbord30@yandex.ru>
2020-07-26 15:07:56 +0300

    Add http client wrapper

---
+++ TenaxUtils/Http/HttpClientWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TenaxUtils.Http
+{
+    public class HttpClientWrapper
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpClientWrapper(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<StringHttpResponse> SendAsync(HttpMethod method, Uri uri, HttpContent content = null)
+        {
+            using (var request = new HttpRequestMessage(method, uri))
+            {
+                if (content != null)
+                {
+                    request.Content = content;
+                }
+
+                using (var response = await _httpClient.SendAsync(request))
+                {
+                    return new StringHttpResponse
+                    {
+                        StatusCode = response.StatusCode,
+                        Content = await response.Content.ReadAsStringAsync()
+                    };
+                }
+            }
+        }
+    }
+}
