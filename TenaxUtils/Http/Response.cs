252366189564df7b63ca8448ec7e8eb7304319b1
Tenax <mrbord30@yandex.ru>
2020-07-26 15:07:43 +0300

    Add http response

---
+++ TenaxUtils/Http/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace TenaxUtils.Http
+{
+    public class HttpResponse<T>
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public T Content { get; set; }
+    }
+
+    public class StringHttpResponse : HttpResponse<string> { }
+}
