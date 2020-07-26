8b71fbcb7c212e124dd1c9cf4e2870e6ab9c37a1
Tenax <mrbord30@yandex.ru>
2020-07-26 15:47:17 +0300

    Add the ability to return multiple content types in the response

--- TenaxUtils/Http/HttpClientWrapper.cs
+++ TenaxUtils/Http/HttpClientWrapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -13,8 +14,17 @@ namespace TenaxUtils.Http
             _httpClient = httpClient;
         }
 
-        public async Task<StringHttpResponse> SendAsync(HttpMethod method, Uri uri, HttpContent content = null)
+        public async Task<HttpResponse<T>> SendAsync<T>(HttpMethod method, Uri uri, HttpContent content = null)
+            where T:class
         {
+            if (typeof(T) != typeof(string) && typeof(T) != typeof(byte[])
+                                            && typeof(T) != typeof(Stream))
+            {
+                throw new NotSupportedException($"Type {nameof(T)} is not supported.");
+            }
+            
+            HttpResponse<T> res = new HttpResponse<T>();
+
             using (var request = new HttpRequestMessage(method, uri))
             {
                 if (content != null)
@@ -24,13 +34,25 @@ namespace TenaxUtils.Http
 
                 using (var response = await _httpClient.SendAsync(request))
                 {
-                    return new StringHttpResponse
+                    res.StatusCode = response.StatusCode;
+
+                    if (typeof(T) == typeof(string))
                     {
-                        StatusCode = response.StatusCode,
-                        Content = await response.Content.ReadAsStringAsync()
-                    };
+                        res.Content = await response.Content.ReadAsStringAsync() as T;
+                    }
+                    else if (typeof(T) == typeof(byte[]))
+                    {
+                        res.Content = await response.Content.ReadAsByteArrayAsync() as T;
+                    }
+                    else if(typeof(T) == typeof(Stream))
+                    {
+                        res.Content = await response.Content.ReadAsStreamAsync() as T;
+                    }
+
+                    return res;
                 }
             }
         }
+
     }
 }
